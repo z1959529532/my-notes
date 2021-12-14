8a6d11ad4d0737ae400871ba92d15b9d7fe83857
zhangyusheng <z1959529532@163.com>
2021-12-14 19:09:50 +0800

    feat: 配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,5 +8,27 @@ module.exports = {
     themeConfig: {
         /** 4、logo图片 */
         logo: '/assets/imgs/header.png',
+    
+        /** 4、导航栏 */
+        nav: navConfig,
+    
+        /** 5、侧边栏 */
+        sidebar: sidebarConfig,
+    
+        /** 7、更新时间 */
+        lastUpdated: '更新时间'
     },
+    
+    /** 6、SEO配置  9、pwa例子配置 */
+    title: '雨生灬wait',
+    description: 'zys笔记',
+    port: '8787',
+    head: headConfig,
+    
+    /** 插件 */
+    plugins: pluginsConfig,
+    
+    /** 8、发布github */
+    /** 配置base */
+    base: '/my-notes/'
 };
