b0bd77196703f586ab87f6ccc7227bff728a3f0f
zhangyusheng <z1959529532@163.com>
2021-12-16 00:46:15 +0800

    feat:

--- docs/.vuepress/config/secret.js
+++ docs/.vuepress/config/secret.js
@@ -1,7 +1,7 @@
 
 /** 隐藏的配置信息 */
 module.exports = {
-    /** vssue配置 */
+    /** vssue配置 */ 
     clientId: 'ceae582190d0dd56db2a',
     clientSecret: '2d7ffe59ff3b7afafabf3fc280fbb3bd114b26bb'
 };
